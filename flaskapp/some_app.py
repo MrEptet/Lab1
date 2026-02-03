d08c1d369bd6fbf7c7b6b5899310024854e0a42b
MrEptet <murygin.vlad@mail.ru>
2026-02-03 18:21:33 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -67,3 +67,55 @@ def net():
  # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
  # сети, если был нажат сабмит, либо передадим falsy значения
  return render_template('net.html',form=form,image_name=filename,neurodic=neurodic) 
+
+from flask import request
+from flask import Response
+import base64
+from PIL import Image
+from io import BytesIO
+import json
+# метод для обработки запроса от пользователя
+@app.route("/apinet",methods=['GET', 'POST'])
+def apinet():
+ neurodic = {}
+ # проверяем, что в запросе json данные
+ if request.mimetype == 'application/json':
+  # получаем json данные
+  data = request.get_json()
+  # берем содержимое по ключу, где хранится файл
+  # закодированный строкой base64
+  # декодируем строку в массив байт, используя кодировку utf-8
+  # первые 128 байт ascii и utf-8 совпадают, потому можно
+  filebytes = data['imagebin'].encode('utf-8')
+  # декодируем массив байт base64 в исходный файл изображение
+  cfile = base64.b64decode(filebytes)
+  # чтобы считать изображение как файл из памяти, используем BytesIO
+  img = Image.open(BytesIO(cfile))
+  decode = neuronet.getresult([img])
+  neurodic = {}
+  for elem in decode:
+   neurodic[elem[0][1]] = str(elem[0][2])
+   print(elem)
+ # преобразуем словарь в json-строку
+ ret = json.dumps(neurodic)
+ # готовим ответ пользователю
+ resp = Response(response=ret,
+          status=200,
+          mimetype="application/json")
+ # возвращаем ответ
+ return resp 
+
+import lxml.etree as ET
+@app.route("/apixml",methods=['GET', 'POST'])
+def apixml():
+ #парсим xml файл в dom
+ dom = ET.parse("./static/xml/file.xml")
+ #парсим шаблон в dom
+ xslt = ET.parse("./static/xml/file.xslt")
+ #получаем трансформер
+ transform = ET.XSLT(xslt)
+ #преобразуем xml с помощью трансформера xslt
+ newhtml = transform(dom)
+ #преобразуем из памяти dom в строку, возможно, понадобится указать кодировку
+ strfile = ET.tostring(newhtml)
+ return strfile 
