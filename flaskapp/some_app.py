0c056941212d47a0bbd344e523987d0a810618a1
MrEptet <murygin.vlad@mail.ru>
2026-02-03 18:22:35 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -6,6 +6,10 @@ from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from flask import Flask
 app = Flask(__name__)
+#декоратор для вывода страницы по умолчанию
+@app.route("/")
+def hello():
+ return " <html><head></head> <body> Hello World! </body></html>"
 if __name__ == "__main__":
  app.run(host='127.0.0.1', port = 10000) 
 # используем csrf токен, можете генерировать его сами
