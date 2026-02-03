e3aaf7494d92fe7a74d125529327d8c644a69b12
MrEptet <murygin.vlad@mail.ru>
2026-02-03 21:43:22 +0700

    Update net.py

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -38,10 +38,10 @@ def read_image_files(files_max_count,dir_name):
    files_count = len(files)
   image_box = [[]]*files_count
   for file_i in range(files_count): # читаем изображения в список
-    full_path - os.path.join(dir_name, files[file_i])
+    full_path = os.path.join(dir_name, files[file_i])
     img = Image.open(full_path)
     image_bix[file_i] = img
-    ing.close()
+    img.close()
    #image_box[file_i] = Image.open(dir_name+'/'+files[file_i])
   return files_count, image_box
   
