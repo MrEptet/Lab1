29c25c61460f637321f8ba3ef89ad08ca61ae54f
MrEptet <murygin.vlad@mail.ru>
2026-02-03 16:28:53 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -56,10 +56,10 @@ def net():
  # можете изменить немного код и передать только загруженный файл
   decode = neuronet.getresult(fimage)
  # записываем в словарь данные классификации
- for elem in decode:
- neurodic[elem[0][1]] = elem[0][2]
+  for elem in decode:
+   neurodic[elem[0][1]] = elem[0][2]
  # сохраняем загруженный файл
- form.upload.data.save(filename)
+  form.upload.data.save(filename)
  # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
  # сети, если был нажат сабмит, либо передадим falsy значения
  return render_template('net.html',form=form,image_name=filename,neurodic=neurodic) 
