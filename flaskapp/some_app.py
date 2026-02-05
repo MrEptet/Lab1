979b902b990923d6623eaa985f07ee266ced675b
MrEptet <murygin.vlad@mail.ru>
2026-02-05 18:25:24 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -133,39 +133,122 @@ def apixml():
 '''
 
 import random
-import os
-# модуль работы с изображениями
+from flask_wtf import FlaskForm,RecaptchaField
+from wtforms import StringField, SubmitField, TextAreaField
+# модули валидации полей формы
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from flask import Flask, Response, render_template
+import base64
 from PIL import Image
-import numpy as np
+from io import BytesIO
+import json
+import lxml.etree as ET
+import os
+import sys
+# from . import net as neuronet # <-- Импорт модуля neuronet удален
+import gc
+# модуль проверки и преобразование имени файла для устранения в имени символов типа / и т.д.
+from werkzeug.utils import secure_filename
 
-# Конфигурация GPU, Keras и TensorFlow удалены
+app = Flask(__name__)
+# декоратор для вывода страницы по умолчанию
+@app.route("/")
+def hello():
+ return " <html><head></head> <body> Hello World! </body></html>"
+if __name__ == "__main__":
+ app.run(host='127.0.0.1', port = 10000) 
+# используем csrf токен, можете генерировать его сами
+SECRET_KEY = 'secret'
+app.config['SECRET_KEY'] = SECRET_KEY
+# используем капчу и полученные секретные ключи с сайта Google
+app.config['RECAPTCHA_USE_SSL'] = False
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdODV8sAAAAAAd3JTTL9Few1qPix-q0ijaRfVIC'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdODV8sAAAAAMHonXgrsII-IA20FuBY_8hGBZgw'
+app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
+# обязательно добавить для работы со стандартными шаблонами
+from flask_bootstrap import Bootstrap
+bootstrap = Bootstrap(app)
+# создаем форму для загрузки файла
+class NetForm(FlaskForm):
+ openid = StringField('openid', validators = [DataRequired()])
+ upload = FileField('Load image', validators=[
+ FileRequired(),
+ FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+ recaptcha = RecaptchaField()
+ submit = SubmitField('send')
 
-height = 224
-width = 224
+# метод обработки запроса GET и POST от клиента
+@app.route("/net",methods=['GET', 'POST'])
+def net():
+ # создаем объект формы
+ form = NetForm()
+ # обнуляем переменные, передаваемые в форму
+ filename=None
+ neurodic = None # Инициализация как None, чтобы шаблон знал, что данных пока нет
 
-# чтение изображений из каталога
-def read_image_files(files_max_count,dir_name):
-  files = os.listdir(dir_name)
-  files_count = files_max_count
-  if(files_max_count>len(files)): # определяем количество файлов не больше max
-   files_count = len(files)
-  image_box = [[]]*files_count
-  for file_i in range(files_count): # читаем изображения в список
-    full_path = os.path.join(dir_name, files[file_i])
-    img = Image.open(full_path)
-    img.load() # Принудительно загружаем данные в память
-    image_box[file_i] = img
-  return files_count, image_box
+ # проверяем нажатие сабмит и валидацию введенных данных
+ if form.validate_on_submit():
+  # файлы с изображениями читаются из каталога static
+  base_dir = os.path.dirname(os.path.abspath(__file__))
+  static_path = os.path.join(base_dir, 'static')
   
-def getResult(image_box):
-  files_count = len(image_box)
-  images_resized = [[]]*files_count
-  # нормализуем изображения и преобразуем в numpy
-  for i in range(files_count):
-   # Преобразование в numpy массив и нормализация до диапазона [0.0, 1.0]
-   images_resized[i] = np.array(image_box[i].resize((height,width)))/255.0
-  images_resized = np.array(images_resized)
+  filename = os.path.join(static_path, secure_filename(form.upload.data.filename))
+ 
+  # Логика нейросети полностью удалена.
+  # Вместо этого можно добавить другую логику обработки файла или просто сохранить его.
   
-  # Код, связанный с нейросетью (resnet.predict и decode_predictions), удален.
-  # Функция теперь возвращает numpy-массив изображений
-  return images_resized
+  # Сохраняем загруженный файл
+  form.upload.data.save(filename)
+  
+  # В этом месте neurodic остается None, если вы не добавите другую логику
+
+ # передаем форму в шаблон, так же передаем имя файла и результат работы 
+ # (neurodic будет None при GET-запросе или после обработки без результатов)
+ gc.collect()
+ return render_template('net.html',form=form,image_name=filename,neurodic=neurodic) 
+
+from flask import request
+# метод для обработки запроса от пользователя
+@app.route("/apinet",methods=['GET', 'POST'])
+def apinet():
+ neurodic = None
+ # проверяем, что в запросе json данные
+ if request.mimetype == 'application/json':
+  data = request.get_json()
+  filebytes = data['imagebin'].encode('utf-8')
+  cfile = base64.b64decode(filebytes)
+  img = Image.open(BytesIO(cfile))
+  
+  # Логика нейросети удалена. neurodic остается None.
+  
+  # Если вам нужен ответ в формате JSON, но без результатов НС, 
+  # можно вернуть просто статус OK:
+  neurodic = {"status": "image received and processed"}
+
+ # преобразуем словарь в json-строку (если он был инициализирован)
+ if neurodic is not None:
+     ret = json.dumps(neurodic)
+ else:
+     ret = json.dumps({"status": "no data processed"})
+     
+ # готовим ответ пользователю
+ resp = Response(response=ret,
+          status=200,
+          mimetype="application/json")
+ # возвращаем ответ
+ return resp 
+ 
+@app.route("/apixml",methods=['GET', 'POST'])
+def apixml():
+ #парсим xml файл в dom
+ dom = ET.parse("./static/xml/file.xml")
+ #парсим шаблон в dom
+ xslt = ET.parse("./static/xml/file.xslt")
+ #получаем трансформер
+ transform = ET.XSLT(xslt)
+ #преобразуем xml с помощью трансформера xslt
+ newhtml = transform(dom)
+ #преобразуем из памяти dom в строку, возможно, понадобится указать кодировку
+ strfile = ET.tostring(newhtml)
+ return strfile
