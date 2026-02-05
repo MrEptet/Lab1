742590f319ae03fd09c2b52d0ed5365c438de532
MrEptet <murygin.vlad@mail.ru>
2026-02-06 02:30:41 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -129,7 +129,7 @@ def net():
 
         # 2. Создаем измененное изображение
         modified_image_np = apply_checkerboard(original_image_np, percentage)
-        modified_image_unit8 = (modified_image_np * 255).astype(np.unit8)
+        modified_image_unit8 = (modified_image_np * 255).astype(np.uint8)
         modified_image = Image.fromarray(modified_image_unit8, 'RGB')
         
         # Сохраняем измененное изображение
