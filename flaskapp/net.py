7876921e10ac5f91c05fe71dfacb58a14d71b472
MrEptet <murygin.vlad@mail.ru>
2026-02-05 18:07:56 +0700

    Update net.py

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -1,3 +1,4 @@
+'''
 import random
 # библиотека keras для НС
 import keras
@@ -58,3 +59,43 @@ def getResult(image_box):
   # декодируем ответ сети в один распознанный класс top=1 (можно больше классов)
   decode = decode_predictions(out_net, top=1)
   return decode 
+'''
+
+import random
+import os
+# модуль работы с изображениями
+from PIL import Image
+import numpy as np
+# Конфигурация GPU удалена, так как не нужна без TensorFlow/Keras
+
+height = 224
+width = 224
+
+# чтение изображений из каталога
+# учтите, если там есть файлы, не соответствующие изображениям, или каталоги
+# возникнет ошибка
+def read_image_files(files_max_count,dir_name):
+  files = os.listdir(dir_name)
+  files_count = files_max_count
+  if(files_max_count>len(files)): # определяем количество файлов не больше max
+   files_count = len(files)
+  image_box = [[]]*files_count
+  for file_i in range(files_count): # читаем изображения в список
+    full_path = os.path.join(dir_name, files[file_i])
+    img = Image.open(full_path)
+    img.load()
+    image_box[file_i] = img
+  return files_count, image_box
+  
+def getResult(image_box):
+  files_count = len(image_box)
+  images_resized = [[]]*files_count
+  # нормализуем изображения и преобразуем в numpy
+  for i in range(files_count):
+   # Преобразование в numpy массив и нормализация до диапазона [0.0, 1.0]
+   images_resized[i] = np.array(image_box[i].resize((height,width)))/255.0
+  images_resized = np.array(images_resized)
+  
+  # Код, связанный с resnet.predict и decode_predictions, удален.
+  # Функция теперь возвращает numpy-массив изображений
+  return images_resized
